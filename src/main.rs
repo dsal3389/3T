bc87e6901132ab035dbbe82ef389d496c76c5669
daniel sonbolian <dsal3389@gmail.com>
2025-07-14 21:54:22 +0300

    REWRITEEEE, the exact thing I wanted to write is already written
    ughhh, bluetui

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,7 @@
-use ratatui::DefaultTerminal;
-
-mod logging;
-mod views;
-mod event;
-mod app;
-
-use app::App;
-
-fn setup_logger() {
-    let logger = logging::AppLogger::from_path("bb.log").expect("couldn't create logger");
-    log::set_boxed_logger(Box::new(logger))
-        .map(|()| log::set_max_level(log::LevelFilter::Debug)).unwrap();
-}
-
-async fn run(terminal: DefaultTerminal) -> anyhow::Result<()> {
-    let app = App::new();
-    app.run(terminal).await
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    setup_logger();
-
-    let terminal = ratatui::init();
-    let result = run(terminal).await;
-    ratatui::restore();
-    result
+    println!("waa waat");
+    Ok(())
 }
+
